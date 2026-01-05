6735c61e1f5dafefcd24a61f3384b3f1c7bc4b1f
Noah Ross <noahrossnz@gmail.com>
2026-01-05 20:33:03 +1300

    changed database to use tinyint

--- application/backend/src/backend.cpp
+++ application/backend/src/backend.cpp
@@ -2,7 +2,7 @@
 
 namespace Backend{
     Backend::Backend(): db("main.db", SQLite::OPEN_READWRITE | SQLite::OPEN_CREATE){
-        db.exec("CREATE TABLE IF NOT EXISTS statBlocks(name TEXT, armorClass INTEGER, hitPoint INTEGER, str INTEGER, dex INTEGER, con INTEGER, int INTEGER, wis INTEGER, cha INTEGER)");
+        db.exec("CREATE TABLE IF NOT EXISTS statBlocks(name TEXT, armorClass TINYINT, hitPoint TINYINT, str TINYINT, dex TINYINT, con TINYINT, int TINYINT, wis TINYINT, cha TINYINT)");
     }
 
     std::vector<StatBlock> Backend::GetAllStatBlocks(){
@@ -11,14 +11,14 @@ namespace Backend{
         StatBlock temp;
         while(getAll.executeStep()){
             temp.name = getAll.getColumn("name").getString();
-            temp.armorClass = getAll.getColumn("armorClass").getInt();
-            temp.hitPoint = getAll.getColumn("hitPoint").getInt();
-            temp.strength = getAll.getColumn("str").getInt();
-            temp.dexterity = getAll.getColumn("dex").getInt();
-            temp.constitution = getAll.getColumn("con").getInt();
-            temp.intellgence = getAll.getColumn("int").getInt();
-            temp.wisdom = getAll.getColumn("wis").getInt();
-            temp.charisma = getAll.getColumn("cha").getInt();
+            temp.armorClass = getAll.getColumn("armorClass").getUInt();
+            temp.hitPoint = getAll.getColumn("hitPoint").getUInt();
+            temp.strength = getAll.getColumn("str").getUInt();
+            temp.dexterity = getAll.getColumn("dex").getUInt();
+            temp.constitution = getAll.getColumn("con").getUInt();
+            temp.intellgence = getAll.getColumn("int").getUInt();
+            temp.wisdom = getAll.getColumn("wis").getUInt();
+            temp.charisma = getAll.getColumn("cha").getUInt();
             list.push_back(temp);
         }
         return list;
